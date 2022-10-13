cecc1e865fb98d848f1c3263d4bf4385a3207a97
Peyton Bechard <pbechard@my.yorku.ca>
2022-10-13 20:41:53 +0800

    Adds options areas to new components with some styling

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import EnterGrades from './EnterGrades';
 import ManageGrades from './ManageGrades';
 
--- src/components/ManageGrades.tsx
+++ src/components/ManageGrades.tsx
@@ -19,51 +19,44 @@ const ManageGrades: React.FC<Props> = ({
   };
 
   return (
-    <div className='enter-grades-container'>
-      <select id='quarter-selection' defaultValue=''>
-        <option disabled value=''>
-          Select a Quarter
-        </option>
-        <option value='q1'>Q1</option>
-        <option value='q2'>Q2</option>
-        <option value='q3'>Q3</option>
-        <option value='q4'>Q4</option>
-      </select>
-      <select
-        id='subject-selection'
-        ref={subjectRef}
-        defaultValue=''
-        onChange={handleSubjectChange}
-      >
-        <option disabled value=''>
-          Select a Subject
-        </option>
-        {getSubjects.map((subject) => {
-          return (
-            <option
-              key={subject.replaceAll(' ', '-').toLowerCase()}
-              value={subject}
-            >
-              {subject}
+    <div className='manage-grades-container'>
+      <div className='manage-options-area'>
+        <div className='manage-quarter-selection-area'>
+          <span className='label'>Apply to:</span>
+          <input type='checkbox' value='q1' id='manage-q1' />
+          <label htmlFor='manage-q1'>Q1</label>
+          <input type='checkbox' value='q2' id='manage-q2' />
+          <label htmlFor='manage-q2'>Q2</label>
+          <input type='checkbox' value='q3' id='manage-q3' />
+          <label htmlFor='manage-q3'>Q3</label>
+          <input type='checkbox' value='q4' id='manage-q4' />
+          <label htmlFor='manage-q4'>Q4</label>
+          <input type='checkbox' value='all' id='manage-q-all' />
+          <label htmlFor='manage-q-all'>All</label>
+        </div>
+
+        <div className='manage-subject-selection'>
+          <select
+            ref={subjectRef}
+            defaultValue=''
+            onChange={handleSubjectChange}
+          >
+            <option disabled value=''>
+              Select a Subject
             </option>
-          );
-        })}
-      </select>
-      <select id='category-selection' ref={categoryRef} defaultValue=''>
-        <option disabled value=''>
-          Select a Category
-        </option>
-        {getCategories.map((category) => {
-          return (
-            <option
-              key={category.replaceAll(' ', '-').toLowerCase()}
-              value={category}
-            >
-              {category}
-            </option>
-          );
-        })}
-      </select>
+            {getSubjects.map((subject) => {
+              return (
+                <option
+                  key={subject.replaceAll(' ', '-').toLowerCase()}
+                  value={subject}
+                >
+                  {subject}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
